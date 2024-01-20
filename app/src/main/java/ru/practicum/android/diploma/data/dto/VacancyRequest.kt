22b34ca7d7f2a8565fccd2f21a828329586589dc
Bugrov1 <px316@yandex.ru>
2024-01-20 23:46:37 +0300

    0 Network Interface

---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyRequest.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.data.dto
+
+data class VacancyRequest(
+    val text: String,
+    val area: String?,
+    val page_limit: Int,
+    val showSalary: Boolean,
+    val industry: String?,
+    val salary: Int?,
+) {
+    fun request(): HashMap<String, String> {
+        val options: HashMap<String, String> = HashMap()
+        options["text"] = text
+        options["per_page"] = page_limit.toString()
+        if (area != null) {
+            options["area"] = area
+        }
+        if (industry != null) {
+            options["area"] = industry
+        }
+        if (salary != null) {
+            options["salary"] = salary.toString()
+        }
+        options["only_with_salary"] = showSalary.toString()
+        return options
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/NetworkClient.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.data.network
+
+import ru.practicum.android.diploma.data.dto.VacancyResponse
+
+interface NetworkClient {
+    suspend fun doRequest(request:Map<String, String>): VacancyResponse
+}
