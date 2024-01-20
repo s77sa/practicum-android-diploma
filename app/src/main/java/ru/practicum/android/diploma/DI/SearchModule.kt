425eda9e0794e4181148f3830571788865261963
Bugrov1 <px316@yandex.ru>
2024-01-21 02:29:23 +0300

    0_Retrofit_Api2 RetrofitNetworkClient added fix 2

---
+++ app/src/main/java/ru/practicum/android/diploma/DI/SearchModule.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.DI
+
+
+import com.google.gson.Gson
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.network.HHApi
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+
+val searchModule = module {
+
+    single<HHApi> {
+        Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApi::class.java)
+    }
+    factory { Gson() }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(context = get(), hhApi = get())
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/App.kt
@@ -0,0 +1,22 @@
+package ru.practicum.android.diploma.ui.root
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import ru.practicum.android.diploma.DI.searchModule
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@App)
+            modules(listOf(searchModule))
+        }
+    }
+}
+
