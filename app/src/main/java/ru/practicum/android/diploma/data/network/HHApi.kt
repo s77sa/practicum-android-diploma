1c4484d79cd093671ee6dd405aa9ff645c75e8d1
Bugrov1 <px316@yandex.ru>
2024-01-21 00:57:48 +0300

    0_Retrofit_Api

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HHApi.kt
@@ -0,0 +1,22 @@
+package ru.practicum.android.diploma.data.network
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.QueryMap
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.data.dto.VacancyResponse
+
+const val ACCESS_TOKEN = BuildConfig.HH_ACCESS_TOKEN
+
+interface HHApi {
+    @Headers(
+        "Authorization: Bearer $ACCESS_TOKEN",
+        "HH-User-Agent: HHPracticum (px316@yandex.ru)"
+    )
+    @GET("/vacancies/")
+    suspend fun search(
+        @QueryMap params: Map<String, String>
+    ): Response<VacancyResponse>
+}
+
