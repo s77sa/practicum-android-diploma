bc268a5a4cec9da0f9091719b35c92cd2eeb47bb
Git_maalmi <marginali@ya.ru>
2024-01-23 23:53:02 +0300

    Добавил layout_search_vacancy.xml & VacanciesAdapter и Holder

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/VacancyViewHolder.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.presentation.search
+
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.LayoutSearchVacancyBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+
+class VacancyViewHolder (private val binding: LayoutSearchVacancyBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(vacancy: Vacancy) {
+
+            binding.name.text = vacancy.name
+            binding.city.text = vacancy.city
+            binding.employer.text = vacancy.employer
+            binding.salaryFrom.text = vacancy.salaryFrom.toString()
+            binding.salaryTo.text = vacancy.salaryTo.toString()
+            binding.salaryCurrency.text = vacancy.salaryCurrency
+
+        Glide.with(itemView)
+            .load(vacancy.employerLogoUrl)
+            .placeholder(R.drawable.ic_main_menu) // ToDo заменить на плэйсхолдер работодателя
+            .centerCrop()
+            .into(binding.logo)
+    }
+}
