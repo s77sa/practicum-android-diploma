39d390041b2223b446bf4c410f23e9eb626a12e9
s77sa <s77sa@yandex.ru>
2024-01-25 15:45:31 +0300

    Исправил некоторые недочеты, но есть баг

--- app/src/main/java/ru/practicum/android/diploma/presentation/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/SearchFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.liveData
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
