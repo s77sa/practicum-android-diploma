cd4d2a902d6077e8ccdfa99e40bc82f650fb9a82
harhanov <116550773+harhanov@users.noreply.github.com>
2024-01-27 17:40:38 +0300

    Сделал "рыбу" VacancyFragment, поправил расстояние между верхними значками в макете.

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,18 +2,20 @@ package ru.practicum.android.diploma.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.feature.favourite.domain.usecase.GetAllVacancyUseCase
 import ru.practicum.android.diploma.presentation.favourite.viewmodel.FavouriteFragmentViewModel
 import ru.practicum.android.diploma.presentation.search.viewmodel.SearchViewModel
+import ru.practicum.android.diploma.presentation.viewmodel.VacancyViewModel
 
 val viewModelModule = module {
+
     viewModel<SearchViewModel> {
-        SearchViewModel(searchInteractor = get(), context = get())
+        SearchViewModel()
     }
-    factory<FavouriteFragmentViewModel> {
-        FavouriteFragmentViewModel(get())
+
+    viewModel<FavouriteFragmentViewModel> {
+        FavouriteFragmentViewModel()
     }
-    single {
-        GetAllVacancyUseCase(favouriteRepository = get())
+    viewModel<VacancyViewModel>{
+        VacancyViewModel()
     }
 }
