b3a5b7fcab2377ccb658a790b34d89e5256be3af
Git_maalmi <marginali@ya.ru>
2024-01-27 12:22:53 +0300

    Добавил обработку ввода текста в поле поиска, delay=2s

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -75,6 +75,7 @@ class SearchFragment : Fragment() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 bottomNavigationView?.isVisible = s.isNullOrEmpty()
+                viewModel.searchDebounce(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) = Unit
