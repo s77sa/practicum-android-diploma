40701a5e42c62a9c3fd0b4ea113a547f7c788045
harhanov <116550773+harhanov@users.noreply.github.com>
2024-01-31 15:07:04 +0300

    Сдвинул импорт Intent на строчку ниже

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
@@ -1,7 +1,7 @@
 package ru.practicum.android.diploma.presentation.vacancy
 
-import android.content.Intent
 import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
