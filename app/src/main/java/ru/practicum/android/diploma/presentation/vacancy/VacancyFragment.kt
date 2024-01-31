8d4e199822c57f74cbe8b24f091c8a048db38611
harhanov <116550773+harhanov@users.noreply.github.com>
2024-01-31 17:04:41 +0300

    Update VacancyFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
@@ -1,8 +1,5 @@
 package ru.practicum.android.diploma.presentation.vacancy
 
-
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
