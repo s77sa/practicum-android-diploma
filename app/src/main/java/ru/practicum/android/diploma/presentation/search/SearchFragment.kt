96a0eb75eba028d3847786d9fb01abe5cd141c26
Git_maalmi <marginali@ya.ru>
2024-02-04 00:14:10 +0300

    Убрал дублирование вакансий в списке Добавил снэк бар при пропадании интернета при наличии списка вакансий

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -12,7 +12,6 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -85,12 +84,6 @@ class SearchFragment : Fragment() {
     }
 
     private fun initListeners() {
-        searchInput?.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                // Empty
-            }
-        }
-
         searchInput?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
