996ba56495a2ad10a201c38f6ede65f17ef1737a
harhanov <116550773+harhanov@users.noreply.github.com>
2024-02-04 18:02:29 +0300

    Сделал SelectRegionViewModel и SelectCountryViewModel и стейты к ним.

--- app/src/main/java/ru/practicum/android/diploma/domain/api/FiltersInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FiltersInteractor.kt
@@ -1,4 +1,14 @@
 package ru.practicum.android.diploma.domain.api
 
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.domain.models.Region
+import ru.practicum.android.diploma.presentation.util.Resource
+
 interface FiltersInteractor {
+    fun getCountries(): Flow<Resource<List<Country>>>
+    fun getRegions(countryId: String): Flow<Resource<List<Region>>>
+    fun applyCountryFilter(country: Country)
+    fun applyRegionFilter(region:Region)
+    fun searchRegionByName(regionName: String): Flow<Resource<List<Region>>>
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewModel/SelectCountryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewModel/SelectCountryViewModel.kt
@@ -1,6 +1,53 @@
 package ru.practicum.android.diploma.presentation.filters.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.api.FiltersInteractor
+import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.presentation.filters.states.CountrySelectionState
+import ru.practicum.android.diploma.presentation.util.Resource
 
-class SelectCountryViewModel() : ViewModel() {
+class SelectCountryViewModel(
+    private val filtersInteractor: FiltersInteractor
+) : ViewModel() {
+
+    private val _countrySelectionState = MutableLiveData<CountrySelectionState>()
+    val countrySelectionState: LiveData<CountrySelectionState> get() = _countrySelectionState
+
+    private var selectedCountry: String = ""
+
+    private fun getCountries() {
+        _countrySelectionState.value = CountrySelectionState.Loading
+        viewModelScope.launch {
+            filtersInteractor.getCountries().collect { resource ->
+                when (resource) {
+                    is Resource.Success -> {
+                        val countries = resource.data ?: emptyList()
+                        if (countries.isNotEmpty()) {
+                            _countrySelectionState.value = CountrySelectionState.Success(countries)
+                        } else {
+                            _countrySelectionState.value = CountrySelectionState.NoData
+                        }
+                    }
+                    is Resource.Error -> {
+                        _countrySelectionState.value = CountrySelectionState.Error
+                    }
+                }
+            }
+        }
+    }
+
+    fun applyCountryFilter(country: Country) {
+        viewModelScope.launch {
+            filtersInteractor.applyCountryFilter(country)
+            getCountries()
+        }
+    }
+
+    fun getSelectedCountry(): String {
+        return selectedCountry
+    }
 }
