95bc56742ef5e112c7ca1e709896b652e5464b8f
s77sa <s77sa@yandex.ru>
2024-02-05 17:04:11 +0300

    Добавил Industry интерактор во ViewModel

--- app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewmodel/SelectIndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewmodel/SelectIndustryViewModel.kt
@@ -3,9 +3,10 @@ package ru.practicum.android.diploma.presentation.filters.viewmodel
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.domain.api.IndustryInteractor
 import ru.practicum.android.diploma.domain.models.FilterSettings
 
-class SelectIndustryViewModel(context: Context) : ViewModel() {
+class SelectIndustryViewModel(context: Context, industryInteractor: IndustryInteractor) : ViewModel() {
 
     private val filterSettingsMutableData = MutableLiveData<FilterSettings>()
     val filterSettingsData get() = filterSettingsMutableData
