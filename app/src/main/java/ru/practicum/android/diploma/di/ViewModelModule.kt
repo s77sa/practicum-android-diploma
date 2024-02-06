deae989508d7ff67369a25ce648f1c985ff53b93
Bugrov1 <px316@yandex.ru>
2024-02-06 22:13:17 +0300

    Фрагмент и вьюмодель

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -13,6 +13,7 @@ import ru.practicum.android.diploma.presentation.filters.viewmodel.SelectIndustr
 import ru.practicum.android.diploma.presentation.filters.viewmodel.SelectRegionViewModel
 import ru.practicum.android.diploma.presentation.filters.viewmodel.SelectWorkplaceViewModel
 import ru.practicum.android.diploma.presentation.search.viewmodel.SearchViewModel
+import ru.practicum.android.diploma.presentation.util.DataTransfer
 import ru.practicum.android.diploma.presentation.vacancy.viewmodel.VacancyViewModel
 
 val viewModelModule = module {
@@ -50,4 +51,6 @@ val viewModelModule = module {
     viewModel<VacancyViewModel> {
         VacancyViewModel(vacancyInteractor = get(), favouriteInteractor = get())
     }
+
+    factory { DataTransfer }
 }
