33e01abcb0b7c0c97ea634f881e365c8e337321a
Git_maalmi <marginali@ya.ru>
2024-02-06 23:36:31 +0300

    Исправил, более двух return не пропускает

--- app/src/main/java/ru/practicum/android/diploma/presentation/util/FiltersCompare.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/util/FiltersCompare.kt
@@ -6,11 +6,10 @@ object FiltersCompare {
 
     // метод получает новые данные фильтра, и сохраненный фильтр
     // и возвращает false - если изменений нет и true - если имеются отличия
-    fun compareFilters(newFilter: Filter, savedFilter: Filter) : Boolean {
-        if (newFilter.area.equals(savedFilter.area)) return false
-        if (newFilter.showSalary.equals(savedFilter.showSalary)) return false
-        if (newFilter.salary?.equals(savedFilter.salary) == true) return false
-        if (newFilter.industry.equals(savedFilter.industry)) return false
-        return true
+    fun compareFilters(newFilter: Filter, savedFilter: Filter): Boolean {
+        return ! (newFilter.area.equals(savedFilter.area) ||
+            newFilter.showSalary == savedFilter.showSalary ||
+            newFilter.salary == savedFilter.salary ||
+            newFilter.industry.equals(savedFilter.industry))
     }
 }
