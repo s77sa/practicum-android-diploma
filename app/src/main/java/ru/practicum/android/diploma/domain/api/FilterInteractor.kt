060966cbf6cec01a5c718a2107c1389db8819856
s77sa <s77sa@yandex.ru>
2024-02-07 14:36:26 +0300

    Вернул методы в interactor

--- app/src/main/java/ru/practicum/android/diploma/domain/api/FilterInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/FilterInteractor.kt
@@ -1,15 +1,18 @@
 package ru.practicum.android.diploma.domain.api
 
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.Country
 import ru.practicum.android.diploma.domain.models.Filter
 import ru.practicum.android.diploma.domain.models.FilterSettings
+import ru.practicum.android.diploma.presentation.util.Resource
 
 interface FilterInteractor {
     fun load(): Filter?
     fun write(filter: Filter)
     fun loadFilterSettings(): FilterSettings?
     fun writeFilterSettings(filter: FilterSettings)
-//    fun getCountries(): Flow<Resource<List<Country>>>
-//    fun applyCountryFilter(country: Country)
-//    fun getSelectedCountry(): Country
-//    fun clearCountryFilter()
+    fun getCountries(): Flow<Resource<List<Country>>>
+    fun applyCountryFilter(country: Country)
+    fun getSelectedCountry(): Country
+    fun clearCountryFilter()
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/impl/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/FilterInteractorImpl.kt
@@ -1,9 +1,12 @@
 package ru.practicum.android.diploma.domain.impl
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.api.FilterInteractor
 import ru.practicum.android.diploma.domain.api.FilterRepository
+import ru.practicum.android.diploma.domain.models.Country
 import ru.practicum.android.diploma.domain.models.Filter
 import ru.practicum.android.diploma.domain.models.FilterSettings
+import ru.practicum.android.diploma.presentation.util.Resource
 
 class FilterInteractorImpl(private val filterRepository: FilterRepository) : FilterInteractor {
     override fun load(): Filter? {
@@ -22,21 +25,21 @@ class FilterInteractorImpl(private val filterRepository: FilterRepository) : Fil
         filterRepository.writeFilterSettings(filter)
     }
 
-//    override fun getCountries(): Flow<Resource<List<Country>>> {
-//        TO DO(NOT_IMPLEMENTED_YET)
-//    }
-//
-//    override fun applyCountryFilter(country: Country) {
-//        TO DO(NOT_IMPLEMENTED_YET)
-//    }
-//
-//    override fun getSelectedCountry(): Country {
-//        TO DO(NOT_IMPLEMENTED_YET)
-//    }
-//
-//    override fun clearCountryFilter() {
-//        TO DO(NOT_IMPLEMENTED_YET)
-//    }
+    override fun getCountries(): Flow<Resource<List<Country>>> {
+        TODO(NOT_IMPLEMENTED_YET)
+    }
+
+    override fun applyCountryFilter(country: Country) {
+        TODO(NOT_IMPLEMENTED_YET)
+    }
+
+    override fun getSelectedCountry(): Country {
+        TODO(NOT_IMPLEMENTED_YET)
+    }
+
+    override fun clearCountryFilter() {
+        TODO(NOT_IMPLEMENTED_YET)
+    }
 
     companion object {
         private const val NOT_IMPLEMENTED_YET = "Not yet implemented"
